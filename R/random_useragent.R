4210ef2797c4ca761ce02873db22a56018a826ad
fangzhou-xie <fangzhou.xie@nyu.edu>
2021-06-15 20:42:30 -0400

    main func

---
+++ R/random_useragent.R
@@ -0,0 +1,67 @@
+#' randomly choosing an user-agent string, given a set of conditions
+#'
+#' @param min_obs minimum number observed for any given UA string, default 5000
+#' @param software_name e.g. "Chrome", "Safari", "Firefox", "Edge", etc
+#' @param software_type e.g. "browser", "bot", "application"
+#' @param operating_system_name e.g. "Windows", "Linux", "Mac OS X", "macOS", etc
+#' @param layout_engine_name e.g. "Gecko", "Blink", etc
+#' @return a random user agent string, given filter conditions
+#' @examples
+#' random_useragent()
+random_useragent <- function(
+  min_obs = 5000,
+  software_name = NULL,
+  software_type = "browser",
+  operating_system_name = "Windows",
+  layout_engine_name = NULL
+) {
+
+  ua_strings <- filter_useragent(
+    min_obs = min_obs,
+    software_name = software_name,
+    software_type = software_type,
+    operating_system_name = operating_system_name,
+    layout_engine_name = layout_engine_name
+  )
+  sample(ua_strings, 1)
+}
+
+#' filter useragents based on criteria
+#'
+#' @param min_obs minimum number observed for any given UA string, default 5000
+#' @param software_name e.g. "Chrome", "Safari", "Firefox", "Edge", etc
+#' @param software_type e.g. "browser", "bot", "application"
+#' @param operating_system_name e.g. "Windows", "Linux", "Mac OS X", "macOS", etc
+#' @param layout_engine_name e.g. "Gecko", "Blink", etc
+#' @return character vector of user agents
+#' @examples
+#' filter_useragent(
+#'   min_obs = 500000,
+#'   software_name = "Chrome",
+#'   operating_system_name = "Linux"
+#' )
+filter_useragent <- function(
+  min_obs = 0,
+  software_name = NULL,
+  software_type = NULL,
+  operating_system_name = NULL,
+  layout_engine_name = NULL
+) {
+
+  # filters <- list(software_name = "Chrome", software_type = "browser", operating_system_name = "Windows")
+  uas <- dplyr::filter(useragents, times_seen >= min_obs)
+
+  if (!is.null(software_name)) {
+    uas <- dplyr::filter(uas, software_name %in% !!software_name)
+  }
+  if (!is.null(software_type)) {
+    uas <- dplyr::filter(uas, software_type %in% !!software_type)
+  }
+  if (!is.null(operating_system_name)) {
+    uas <- dplyr::filter(uas, operating_system_name %in% !!operating_system_name)
+  }
+  if (!is.null(layout_engine_name)) {
+    uas <- dplyr::filter(uas, layout_engine_name %in% !!layout_engine_name)
+  }
+  dplyr::pull(uas, user_agent)
+}
--- data-raw/basic_dataset.R
+++ data-raw/basic_dataset.R
@@ -2,7 +2,17 @@ library(tidyverse)
 
 useragents <- read_csv(
   "~/Downloads/whatismybrowser-user-agent-database-2021-05-03-06-17--csv-SAMPLE/whatismybrowser-user-agent-database.csv",
-  col_types = cols(.default = "c")
+  col_types = cols_only(
+    id = col_integer(),
+    user_agent = col_character(),
+    times_seen = col_integer(),
+    software_name = col_character(),
+    software_type = col_character(),
+    operating_system_name = col_character(),
+    hardware_type = col_character(),
+    layout_engine_name = col_character(),
+    last_seen_at = col_character()
+    )
   ) %>%
   select(
     id,
@@ -16,4 +26,4 @@ useragents <- read_csv(
     last_seen_at
   )
 
-usethis::use_data(useragents, internal = TRUE)
+usethis::use_data(useragents, internal = TRUE, overwrite = TRUE)
